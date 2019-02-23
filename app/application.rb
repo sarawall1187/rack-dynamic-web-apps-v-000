482c2c4bd13dd459ae008d384b4c8ad6d03256c2
Sara Walker <sarawall1187@gmail.com>
2019-02-23 16:45:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    num_1 = Kernal.rand(1..20)
+    num_2 = Kernal.rand(1..20)
+    num_3 = Kernal.rand(1..20)
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "You Win."
+    else
+      resp.write "You Lose."
+    end
     resp.finish
   end
 
