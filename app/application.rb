0b5d0e6af0a5ee2d7041bb95deff87608d1aa13d
Sara Walker <sarawall1187@gmail.com>
2019-02-23 16:46:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,8 @@ class Application
     num_1 = Kernal.rand(1..20)
     num_2 = Kernal.rand(1..20)
     num_3 = Kernal.rand(1..20)
+    resp.write "#{num_1}\n"
+    
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win."
     else
